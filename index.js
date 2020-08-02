eb03688ca64aa28ff3b7fd30fecde37bb0c50bff
LeonorPDX <leonoracolbert@gmail.com>
2020-08-02 16:08:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(username, email) {
+    let data = {
+        name: username,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify(data)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML += object.id
+        })
+        .catch(function(error) {
+            document.body.innerHTML += " Unauthorized Access"
+        });
+}

953c92cb1c213defece39070d1abb33afc70f074
hmcdowell95 <hmcdowell95@github.com>
2020-08-11 14:56:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData (a, b) {
+    let user = {name: a, email: b};
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(user)
+      };
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {return response.json();})
+    .then(function(object) {showId(object);})
+    .catch(function(error) {
+        document.body.append(error.message);
+      });
+}
+
+function showId(a) {
+    let b = a.id;
+    document.body.append(b)
+}

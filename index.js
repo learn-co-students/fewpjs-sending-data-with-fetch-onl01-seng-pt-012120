8fb51adf39c43e9f9be779504af87e326fcfa4a0
javers <joeychavira2016@gmail.com>
2020-08-11 19:22:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your code here
-function submitData( name, email ) {
+function submitData(name, email) {
   return fetch( 'http://localhost:3000/users', {
       method: "POST",
       headers: {
